999b9c38e8e44c08401f5d3f12695a6cfc104200
matan <57196433+spyroy@users.noreply.github.com>
2020-06-03 17:30:25 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-int main() {
-	WarGame::DemoGame demoGame;
-	cout << "The winner is:\n " << demoGame.play() << endl;
-    return 0;
-}
+// int main() {
+// 	WarGame::DemoGame demoGame;
+// 	cout << "The winner is:\n " << demoGame.play() << endl;
+//     return 0;
+// }
